76c15fa5f23734821280fc0f450c8879024919f3
joanscure <joan.leyton08@gmail.com>
2022-06-26 17:22:13 -0500

    first commit

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,18 @@
+-- Just an example, supposed to be placed in /lua/custom/
+
+local M = {}
+
+-- make sure you maintain the structure of `core/default_config.lua` here,
+-- example of changing theme:
+
+M.ui = {
+   theme = "gruvchad",
+}
+
+--  Mapping
+M.general = {
+
+
+}
+
+return M
